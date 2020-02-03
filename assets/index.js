13fded097fd124c3cbf7d8c7c5d90829c2d0637a
JIHYE_PARK <qkrwlgp76@gmail.com>
2020-02-03 13:53:00 +0900

    index.js complete

--- assets/index.js
+++ assets/index.js
@@ -7,11 +7,25 @@ import './index.scss'
 const todoForm = document.querySelector('.todoForm')
 const todoInput = todoForm.querySelector('input')
 
+const pendingList = document.querySelector('.pending-list')
+const finishedList = document.querySelector('.finished-list')
+
 const PENDING = 'pending'
 const FINISHED = 'finished'
 
 let pendingArr
 let finishedArr
+
+const element = (tagName, attrs = {}, childrens = []) => {
+  const el = document.createElement(tagName)
+  for(const attr in attrs) {
+    el.setAttribute(attr, attrs[attr])
+  }
+  for(const child of childrens) {
+    el.appendChild(child)
+  }
+  return el
+}
 
 const getTodo = text => {
   return {
@@ -20,8 +34,69 @@ const getTodo = text => {
   }
 }
 
-const todoMake = () => {
+const setStorage = () => {
   localStorage.setItem(PENDING, JSON.stringify(pendingArr))
+  localStorage.setItem(FINISHED, JSON.stringify(finishedArr))
+}
+
+const pushPending = (todos) => {
+  pendingArr.push(todos)
+  return pendingArr
+}
+
+const pushFinished = (todos) => {
+  finishedArr.push(todos)
+  return finishedArr
+}
+
+const printFormat = (todos) => {
+  const li = element('li')
+  const span = element('span')
+  const delBtn = element('button', {'class': 'delBtn'})
+
+  li.id = todos.id
+  span.innerText = todos.text
+  delBtn.innerText = '❌'
+  delBtn.addEventListener('click', handleDelete)
+
+  li.appendChild(span)
+  li.appendChild(delBtn)
+  return li
+}
+
+const printPending = (todos) => {
+  const format = printFormat(todos)
+  const doneBtn = element('button', {'class': 'doneBtn'})
+
+  doneBtn.innerText = '✅'
+  doneBtn.addEventListener('click', handleDone)
+
+  format.appendChild(doneBtn)
+  pendingList.append(format)
+}
+
+const printFinished = (todos) => {
+  const format = printFormat(todos)
+  const replyBtn = element('button', {'class': 'replyBtn'})
+
+  replyBtn.innerText = '⛔'
+  replyBtn.addEventListener('click', handleReply)
+
+  format.appendChild(replyBtn)
+  finishedList.append(format)
+}
+
+const filterPending = (target) => {
+  return pendingArr = pendingArr.filter(todo => todo.id !== target)
+}
+const filterFinished = (target) => {
+  return finishedArr = finishedArr.filter(todo => todo.id !== target)
+}
+const findPending = (target) => {
+  return pendingArr.find(todo => todo.id === target)
+}
+const findFinished = (target) => {
+  return finishedArr.find(todo => todo.id === target)
 }
 
 const handleSubmit = (e) => {
@@ -29,6 +104,42 @@ const handleSubmit = (e) => {
   const text = todoInput.value
   const todo = getTodo(text)
   todoInput.value = ''
+  pushPending(todo)
+  printPending(todo)
+  setStorage()
+}
+
+const handleDone = (e) => {
+  e.preventDefault()
+  const li = e.target.parentNode
+  li.parentNode.removeChild(li)
+
+  const doneTodo = findPending(li.id)
+  filterPending(li.id)
+  pushFinished(doneTodo)
+  printFinished(doneTodo)
+  setStorage()
+}
+
+const handleDelete = (e) => {
+  e.preventDefault()
+  const li = e.target.parentNode
+  li.parentNode.removeChild(li)
+  filterPending(li.id)
+  filterFinished(li.id)
+  setStorage()
+}
+
+const handleReply = (e) => {
+  e.preventDefault()
+  const li = e.target.parentNode
+  li.parentNode.removeChild(li)
+
+  const replyTodo = findFinished(li.id)
+  filterFinished(li.id)
+  pushPending(replyTodo)
+  printPending(replyTodo)
+  setStorage()
 }
 
 const initLoad = () => {
@@ -36,8 +147,14 @@ const initLoad = () => {
   finishedArr = JSON.parse(localStorage.getItem(FINISHED)) || []
 }
 
-const init = () => {
+const prindLoad = () => {
+  pendingArr.forEach(todos => printPending(todos))
+  finishedArr.forEach(todos => printFinished(todos))
+}
+
+function init () {
   todoForm.addEventListener('submit', handleSubmit)
   initLoad()
+  prindLoad()
 }
 init()
